bfbd3b7e83c4689ec57d781ef3eca39635a715d2
wDaniec <andrzejZdobywca@gmail.com>
2021-05-18 16:10:14 +0200

    Add formatting of resnet50 modules

--- snow.py
+++ snow.py
@@ -8,6 +8,13 @@ import torch.nn.functional as F
 from models import resnet50_delta
 
 
+def iterdict(d):
+  for k,v in d.items():        
+     if isinstance(v, dict):
+         iterdict(v)
+     else:            
+         print (k)
+
 class Snow(nn.Module):
     def __init__(self, scale, out_size):
         super(Snow, self).__init__()
@@ -16,7 +23,7 @@ class Snow(nn.Module):
         self.add_hooks()
         self.freeze_model()
         self.delta_model = resnet50_delta(scale)
-        self.ch_pool = ChannelPool()
+        # self.ch_pool = ChannelPool()
     
     def add_hooks(self):
         for name, module in self.source_model.named_modules():
@@ -30,12 +37,41 @@ class Snow(nn.Module):
         for param in self.source_model.parameters():
             param.requires_grad = False
     
-    def forward(self, x):
-        self.source_model(x)
+    def __ugly_ass_function(self):
+        result_dict = {}
         for k, v in self.source_activations.items():
-            print(k, v.shape, self.ch_pool(v))
-        # x = self.delta_model(x, self.source_activations)
+            v = 1
+            res = k.split('.')
+            if len(res) == 1:
+                result_dict[k] = v
+                continue
+
+            layer_name = res[0] + "_inner"
+            bottleneck_id = res[1]
+            if layer_name not in result_dict:
+                result_dict[layer_name] = {}
+            
+            if len(res) == 2:
+                result_dict[layer_name][bottleneck_id] = v
+                continue
+            bottleneck_id += "_inner"
+            if bottleneck_id not in result_dict[layer_name]:
+                result_dict[layer_name][bottleneck_id] = {}
+            module_name = res[2]
+            if len(res) == 4:
+                module_name = res[2] + res[3]
+            result_dict[layer_name][bottleneck_id][module_name] = v
+        return result_dict
+
+    def __get_feature_maps(self, x):
+        self.source_model(x)
+        feature_map = self.__ugly_ass_function()
         self.source_activations.clear()
+        return feature_map
+
+    def forward(self, x):
+        feature_map = self.__get_feature_maps(x)
+        x = self.delta_model(x, feature_map)
         return x
 
 
@@ -53,18 +89,19 @@ class ChannelPool(nn.Module):
         return result.view(batch_size, self.out_channels, width, height)
     
 
-chp = ChannelPool(16, 2)
+# chp = ChannelPool(16, 2)
 
-A = torch.rand((64, 16, 8, 8))
-res = chp(A)
-print(res.shape)
+# A = torch.rand((64, 16, 8, 8))
+# res = chp(A)
+# concated = torch.cat((res, A), dim=1)
 
-# snow_model = Snow(8, 12)
+snow_model = Snow(8, 12)
+print(snow_model)
 
-# train, valid = get_cifar100()
-# for b in enumerate(train):
-#     idx, (x, y) = b
-#     if idx == 1:
-#         break
-#     outputs = snow_model(x)
+train, valid = get_cifar100()
+for b in enumerate(train):
+    idx, (x, y) = b
+    if idx == 1:
+        break
+    outputs = snow_model(x)
 
